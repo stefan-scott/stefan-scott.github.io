763689d97d7a3ce128db41e7ad0d26725f612b72
stefan-scott <scottst@spsd.sk.ca>
2018-01-24 11:49:53 -0600

    links

--- temp.js
+++ temp.js
@@ -14,52 +14,52 @@ selectedContent = ""
 
 
 entryDate[0] = "1"
-entryContent[0] = "aaaaa"
+entryContent[0] = "<a href = \"https://drive.google.com/file/d/1kYolMF_V8bmpzqDl66Gws2Q1eQhIlCV3/view?usp=sharing\"> Space </a>"
 
 entryDate[1] = " 2"
-entryContent[1] = "bbbbbbbbbb"
+entryContent[1] = "<a href = \"https://drive.google.com/file/d/1kYolMF_V8bmpzqDl66Gws2Q1eQhIlCV3/view?usp=sharing\"> Space </a>"
 
 entryDate[2] = " 3"
-entryContent[2] = "cccccccccc"
+entryContent[2] = "<a href = \"https://drive.google.com/file/d/1kYolMF_V8bmpzqDl66Gws2Q1eQhIlCV3/view?usp=sharing\"> Space </a>"
 
 entryDate[3] = " 4"
-entryContent[3] = "ddddddddd"
+entryContent[3] = "<a href = \"https://drive.google.com/file/d/1kYolMF_V8bmpzqDl66Gws2Q1eQhIlCV3/view?usp=sharing\"> Space </a>"
 
 entryDate[4] = " 5"
-entryContent[4] = "eeeeeeeee"
+entryContent[4] = "<a href = \"https://drive.google.com/file/d/1kYolMF_V8bmpzqDl66Gws2Q1eQhIlCV3/view?usp=sharing\"> Space </a>"
 
 entryDate[5] = " 6"
-entryContent[5] = "ffffffffff"
+entryContent[5] = "<a href = \"https://drive.google.com/file/d/1kYolMF_V8bmpzqDl66Gws2Q1eQhIlCV3/view?usp=sharing\"> Space </a>"
 
 entryDate[6] = " 76"
-entryContent[6] = "ggggggg"
+entryContent[6] = "<a href = \"https://drive.google.com/file/d/1kYolMF_V8bmpzqDl66Gws2Q1eQhIlCV3/view?usp=sharing\"> Space </a>"
 
 entryDate[7] = " 8"
-entryContent[7] = "hhhhhhhh"
+entryContent[7] = "<a href = \"https://drive.google.com/file/d/1kYolMF_V8bmpzqDl66Gws2Q1eQhIlCV3/view?usp=sharing\"> Space </a>"
 
 entryDate[8] = " 9"
-entryContent[8] = "iiiiiiii"
+entryContent[8] = "<a href = \"https://drive.google.com/file/d/1kYolMF_V8bmpzqDl66Gws2Q1eQhIlCV3/view?usp=sharing\"> Space </a>"
 
 entryDate[9] = " 0"
-entryContent[9] = "jjjjjjjjjj"
+entryContent[9] = "<a href = \"https://drive.google.com/file/d/1kYolMF_V8bmpzqDl66Gws2Q1eQhIlCV3/view?usp=sharing\"> Space </a>"
 
 entryDate[10] = " random"
-entryContent[10] = "kkkkkkkkkkk"
+entryContent[10] = "<a href = \"https://drive.google.com/file/d/1kYolMF_V8bmpzqDl66Gws2Q1eQhIlCV3/view?usp=sharing\"> Space </a>"
 
 entryDate[11] = " random"
-entryContent[11] = "lllllll"
+entryContent[11] = "<a href = \"https://drive.google.com/file/d/1kYolMF_V8bmpzqDl66Gws2Q1eQhIlCV3/view?usp=sharing\"> Space </a>"
 
 entryDate[12] = " random"
-entryContent[12] = "mmmmmmmm"
+entryContent[12] = "<a href = \"https://drive.google.com/file/d/1kYolMF_V8bmpzqDl66Gws2Q1eQhIlCV3/view?usp=sharing\"> Space </a>"
 
 entryDate[13] = " random"
-entryContent[13] = "nnnnnnnn"
+entryContent[13] = "<a href = \"https://drive.google.com/file/d/1kYolMF_V8bmpzqDl66Gws2Q1eQhIlCV3/view?usp=sharing\"> Space </a>"
 
 entryDate[14] = " random"
-entryContent[14] = "oooooooo"
+entryContent[14] = "<a href = \"https://drive.google.com/file/d/1kYolMF_V8bmpzqDl66Gws2Q1eQhIlCV3/view?usp=sharing\"> Space </a>"
 
 entryDate[15] = " random"
-entryContent[15] = "ppppppppp"
+entryContent[15] = "<a href = \"https://drive.google.com/file/d/1kYolMF_V8bmpzqDl66Gws2Q1eQhIlCV3/view?usp=sharing\"> Space </a>"
 
 
 
