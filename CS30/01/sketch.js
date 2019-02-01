e46d0059f40566d87f39303b94bf6ba47d95cb2e
stefan-scott <scottst@spsd.sk.ca>
2019-02-01 11:25:49 -0600

    Jan 31
    Added Template Code project 01

---
+++ CS30/01/sketch.js
@@ -0,0 +1,19 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  rect(width/2,height/2,mouseX, mouseY);
+  
+}
+
+
