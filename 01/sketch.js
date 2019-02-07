3ca5a49f6bb6b99b1584738b25a39f8d2ddfa64e
stefan-scott <scottst@spsd.sk.ca>
2019-02-07 11:01:41 -0600

    Demo 1 Feb 7

--- 01/sketch.js
+++ 01/sketch.js
@@ -4,12 +4,22 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
+let x = 45;
+let xSpeed = 6;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER);
+  
 }
 
 function draw() {
-  background(220);
+  background(255);
+  fill(255,204,0);
+  ellipse(x,200, 125);
+  fill(29,143,178);
+  rect(mouseX, 400, 75,75);
+
+  x += xSpeed;
+  if (x > windowWidth) x = -30;
 }
