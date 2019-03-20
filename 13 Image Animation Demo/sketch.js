90341abd7e4698aeca8c5a3ef353c3ac0a9344d5
stefan-scott <scottst@spsd.sk.ca>
2019-03-20 10:25:11 -0600

    Finished Animation Demo

--- 13 Image Animation Demo/sketch.js
+++ 13 Image Animation Demo/sketch.js
@@ -3,26 +3,35 @@
 let lionL, lionR;
 let direction = 1;
 let pinImages = [];
+let counter = 0;
+let speed = 1;  //1 - 8
   //1 - left
   //2 - right
 function preload(){
   lionL = loadImage('assets/lion-left.png');
   lionR = loadImage('assets/lion-right.png');
   for (let i = 0; i < 9; i++){
-    //
-    pinImages.push(loadImage('assets/pin-00.png'));
+    //Load all the images into the array
+    pinImages.push(loadImage("assets/pin-0" + i + ".png"));             
   }
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  
 }
 
 function draw() {
   background(220);
   lions();
-  image(pinImages[0],width/2,height/2);
-  
+  image(pinImages[counter],width/2,height/2);
+
+  speed = map(mouseX, 0, width, 2, 8);
+  print(speed);
+  if (frameCount % int(speed) === 0){
+    counter ++;
+    if (counter > 8) counter = 0;
+  }
 }
 
 function lions(){
