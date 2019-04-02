4487e1ba0d1bdadf2c940c595fd7a21bbefb7213
stefan-scott <scottst@spsd.sk.ca>
2019-04-02 10:46:56 -0600

    Finished Object Demo 2

--- 15 Objects Day 2 Demo/sketch.js
+++ 15 Objects Day 2 Demo/sketch.js
@@ -11,6 +11,8 @@ function mouseClicked(){
 function draw() {
   background(220);
   for (let currentBall of ballObjects){
+    currentBall.move();
+    currentBall.mouseOver();
     currentBall.display();
   }
 }
@@ -23,6 +25,7 @@ class Ball{
     this.xSpeed = random(-5,5);
     this.ySpeed = random(-5,5);
     this.size = 30;
+    this.gravity = 0.1;
   }
 
   //Class Methods
@@ -30,4 +33,34 @@ class Ball{
     ellipseMode(CENTER);
     ellipse(this.x, this.y, this.size, this.size);
   }
+
+  move(){
+    this.x += this.xSpeed;
+    this.y += this.ySpeed;
+    this.ySpeed += this.gravity;
+    
+    //check L/R walls
+    if (this.x < 0 || this.x > width){
+      this.xSpeed *= -1;
+    }
+    if (this.y > height){
+      this.ySpeed *= -0.92;
+      this.y = height;
+    }
+  }
+
+  mouseOver(){
+    let d = dist(this.x,this.y, mouseX, mouseY);
+    if (d < this.size / 2){
+      if(mouseIsPressed && mouseButton === CENTER){
+        this.size += 20;
+      }
+      fill(28,80,199);
+    }
+    else{
+      fill(255);
+    }
+  }
+
+  
 }
