b086ee791ca631a425a7bc0403675847921c311c
stefan-scott <scottst@spsd.sk.ca>
2019-04-08 10:20:45 -0600

    Diagonal Lines Generator in Progress

---
+++ 17 Diagonal Line Generator/sketch.js
@@ -0,0 +1,17 @@
+// Diagonal Line Generator
+const spacing = 50;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function diagonalAscending(x, y, s){
+  line(x-s/2, y+s/2, x+s/2, y-s/2);
+}
+
+function draw() {
+  background(220);
+  for (let x = spacing/2; x < width - spacing/2; x+= spacing){
+    diagonalAscending(x,height/2, spacing );
+  }
+}
