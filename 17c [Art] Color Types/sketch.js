60150bdf5c20231c28e143312a3ac2d2327b4fbe
stefan-scott <scottst@spsd.sk.ca>
2019-04-10 10:42:49 -0600

    finished demo

--- 17c [Art] Color Types/sketch.js
+++ 17c [Art] Color Types/sketch.js
@@ -1,11 +1,25 @@
 // Color Solutions Demo
 let rectWidth = 10;
 let rectHeight = 50;
+let colors = ["#ECCE12", "#D3BB03", "#EF2149", "#900D1E", "#382C18" ];
+//let colors = [color(20,30,40),]
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   drawRowRGB(height * 0.2);
   drawRowHSB(height/2);
+  drawRowCustom(height * 0.8);
+}
+
+function drawRowCustom(yPos){
+  colorMode(RGB);
+  let counter = 0;
+  for (let x = 0; x < width; x+=rectWidth){
+    //fill(colors[counter%5]);
+    fill(colors[int(random(colors.length))]);
+    rect(x,yPos, rectWidth, rectHeight);
+    counter ++;
+  }
 }
 
 function drawRowHSB(yPos){
