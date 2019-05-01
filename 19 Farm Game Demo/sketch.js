384d06a91640814b374216a17c1cdd840f1128a7
stefan-scott <scottst@spsd.sk.ca>
2019-05-01 10:14:54 -0600

    Update sketch.js

--- 19 Farm Game Demo/sketch.js
+++ 19 Farm Game Demo/sketch.js
@@ -1,8 +1,18 @@
 //  Farm Game
+const COLUMNS = 5;
+const ROWS = 5;
+let playerX = 3;
+let playerY = 4;
 
 let tiles = []; //0 - blank
                 //1 - chicken
                 //2 - cow
+
+let level = [ [0, 1, 0, 1, 0],
+              [1, 1, 1 ,0, 0],
+              [0, 0, 1, 1, 0],
+              [1, 0, 0, 1, 1],
+              [0, 0, 1, 2, 1] ];
 
 function preload(){
   for (let i = 0; i < 3; i++){
