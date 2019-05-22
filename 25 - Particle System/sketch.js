07221a7d9155597d783cc561e507061b5b34e895
stefan-scott <scottst@spsd.sk.ca>
2019-05-22 11:01:25 -0600

    Update sketch.js

--- 25 - Particle System/sketch.js
+++ 25 - Particle System/sketch.js
@@ -1,9 +1,16 @@
 // Particle Systems
 let pArray = [];
+let gear;
+
+function preload(){
+  gear = loadImage("/assets/gear.png");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
+  imageMode(CENTER);
+  background(0);
 }
 
 function draw() {
@@ -20,7 +27,7 @@ function draw() {
 }
 
 function mouseClicked(){
-  
+  pArray.push(new Particle(mouseX, mouseY));
 }
 
 class Particle{
@@ -30,17 +37,22 @@ class Particle{
     this.y = y_;
     this.c = color(map(x_,0, width,0,255),map(y_,0, height,0,255),map(x_,0, width,255,0));
     this.size = random(10,20);
-    this.ySpeed = random(-2,1);
-    this.xSpeed = random(-1,1);
+    this.ySpeed = random(-1,1);
+    this.xSpeed = random(-0.5,0.5);
     this.lifetime = int(random(80,160));
     this.maxLifetime = this.lifetime;
-    this.GRAV = 0.1;
+    this.GRAV = -0.02;
+    this.noiseLoc = random(10);
+    this.steps = 0;
   }
 
   move(){
+    this.steps ++;
     this.lifetime -= 1;
     this.ySpeed += this.GRAV;
-    this.x += this.xSpeed;
+    this.x += (map(noise(this.noiseLoc),0,1,-1,1));
+    this.noiseLoc += 0.01;
+    //this.x += this.xSpeed;
     this.y += this.ySpeed;
     this.floorCollision();
   }
@@ -63,7 +75,9 @@ class Particle{
     push();
     translate(this.x,this.y);
     scale(map(this.lifetime,0,this.maxLifetime,0,1));
-    ellipse(0,0,this.size,this.size);
+    rotate(radians(this.steps*3));
+    //ellipse(0,0,this.size,this.size);
+    image(gear,0,0,this.size,this.size);
     pop();
   }
 
