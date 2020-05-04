f538c8c8fc69c0bd47de05dac5dd8ce0e37c1742
Scott <ScottSt@spsd.sk.ca>
2020-05-04 11:21:26 -0600

    Added ThomasFish

--- Fish Tank Project/sketch.js
+++ Fish Tank Project/sketch.js
@@ -10,6 +10,8 @@ const CANVAS_SCALE = 0.3; //use to scale decorations to other sizes of Canvas
 let sandSpots = [];
 let tankDecorations = [];
 
+let fishCollection = [];
+
 function preload() {
   tankDecorations.push(loadImage("assets/tank coral 01.png"));
   tankDecorations.push(loadImage("assets/tank plant 01.png"));
@@ -20,7 +22,9 @@ function preload() {
 
 function setup() {
   createCanvas(800, 500);
-  testFish = new ScottSFish(100);
+  fishCollection.push(new ScottSFish(100));
+  fishCollection.push(new ThomasSFish(45));
+  fishCollection.push(new ThomasSFish(30));
   topColor = color(100, 154, 245);
   bottomColor = color(43, 74, 200);
   initSandSpots();
@@ -29,8 +33,12 @@ function setup() {
 function draw() {
   //background(50);
   drawTank();
-  testFish.move();
-  testFish.display();
+  for(let f of fishCollection){
+    f.compare(fishCollection);
+    f.move();
+    f.display();
+  }
+  
 
   foregroundDecorations(); //should be last thing in draw()
 }
@@ -311,5 +319,133 @@ class ScottSFish extends AnimatedObject {
 
 }
 
+class ThomasSFish extends AnimatedObject{
+  
+  constructor(size_) {
+    
+    super();
 
+    this.sightRange = 75; // Range in pixels the fish will avoid other fish
+    this.bubbleChance = 1; // Percent chance to blow a bubble each frame
+    this.bubbleSize = 1.5; // Size of bubbles blown by this fish, relative to the fish's size
+    this.bubbleRandomness = 0.67; // Decimal percent of the randomness of the bubbles' size
+    this.friction = 1.03; // Speed at which the fish's velocity will slow
+    this.minVel = 0.05;
+    this.maxVel = 8;
+
+    this.size = size_;
+    this.vel = p5.Vector.random2D();
+    this.vel.setMag(random(this.minVel, this.maxVel));
+    this.bubbleArr = []; // Array of bubbles this fish has blown
+  }
+
+  display() {
+    
+    // Draws the fish to the screen
+    push();
+    translate(this.pos.x, this.pos.y);
+    rotate(this.vel.heading());
+    noStroke();
+    fill(27, 85, 76);
+    ellipse(0, 0, this.size, this.size / 1.5);
+    triangle(0, 0, -this.size / 1.4, this.size / 2, -this.size / 1.4, -this.size / 2);
+    fill(0);
+    ellipse(this.size / 2.5, 0, this.size / 5);
+    pop();
+  }
+  
+  move() {
+
+    // Applies friction
+    this.vel.x /= this.friction;
+    this.vel.y /= this.friction;
+
+    if (this.vel.mag() < this.minVel) {
+      // Gives the fish a random velocity once it goes slower than its min velocity
+      this.vel = p5.Vector.random2D();
+      this.vel.setMag(random(this.minVel, this.maxVel));
+    }
+
+    // Makes the fish bounce off the window edges
+    if (this.pos.x + this.vel.x - this.size / 2 < 0 || this.pos.x + this.vel.x + this.size / 2 > width) {
+      this.vel.x *= -1;
+    }
+    if (this.pos.y + this.vel.y - this.size / 2 < 0 || this.pos.y + this.vel.y + this.size / 2 > height) {
+      this.vel.y *= -1;
+    }
+    
+    this.pos.add(this.vel); // Updates the fishes' velocities
+
+    // Code for bubbles
+    this.blowBubble();
+    this.updateBubbles();
+
+  }
+  
+  blowBubble() {
+    
+    // Makes the fish have a chance to blow a bubble
+    if (this.bubbleChance > int(random(0, 99))) {
+      this.bubbleArr.push(new ThomasSFishBubble(this.pos.x, this.pos.y, this.vel.x, this.bubbleSize + random(-this.bubbleRandomness, this.bubbleRandomness), this));
+    }
+  }
+
+  updateBubbles() {
+    
+    // Updates all bubbles related to this fish
+    for (let i = this.bubbleArr.length - 1; i >= 0; i--) {
+      this.bubbleArr[i].display();
+      this.bubbleArr[i].move(i);
+    }
+  }
+
+  compare(objArr) {
+    
+    // Compares this fish against all other fishes
+    for (let j = 0; j < objArr.length; j++) {
+      if (objArr[j] !== this) {
+        if (sqrt(pow(this.pos.x - objArr[j].pos.x, 2) + pow(this.pos.y - objArr[j].pos.y, 2)) < this.sightRange + objArr[j].size) {
+          // If the distance between the two fish is smaller than it's sight range, face away from it
+          this.vel.rotate(-this.vel.heading() + Math.atan2(objArr[j].pos.y - this.pos.y, objArr[j].pos.x - this.pos.x) + radians(180));
+        }
+      }
+    }
+  }
+}
+
+class ThomasSFishBubble extends AnimatedObject {
+
+  constructor(x_, y_, xVel, size_, parentFish_) {
+    
+    super();
+    
+    this.buoyancy = -4; // Speed the bubble will float up
+    this.xVelFriction = 1.03; // Speed the bubble will slow it's initial x velocity
+
+    this.pos = createVector(x_, y_);
+    this.vel = createVector(xVel, this.buoyancy);
+    this.parentFish = parentFish_;
+    this.size = this.parentFish.size / size_;
+  }
+
+  display() {
+    
+    // Draws the bubble to the screen
+    fill(0, 0);
+    stroke(255, 255, 255);
+    strokeWeight(1);
+    ellipse(this.pos.x, this.pos.y, this.size / 2);
+  }
+
+  move(i) {
+    
+    this.pos.add(this.vel); // Updates the position
+    this.vel.x = this.vel.x / this.xVelFriction; // Slows the x velocity by a little due to friction
+
+    if (this.pos.y + this.size / 2 < 0) {
+      // Deletes the bubble if it's above the screen
+      this.parentFish.bubbleArr.splice(i, 1);
+    }
+  }
+}
 
