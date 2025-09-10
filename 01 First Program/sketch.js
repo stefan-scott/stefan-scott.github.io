a9d6ef9e32a7886d7a83b915c4d409d9b66557d7
stefan-scott <scottst@spsd.sk.ca>
2025-09-10 14:09:01 -0600

    ALmost done demo

--- 01 First Program/sketch.js
+++ 01 First Program/sketch.js
@@ -1,13 +1,40 @@
+// Coordinate Systems and User Events
 // Mr. Scott
-// Interactive Programs
+// September 10, 2025
+// A first look at interactive programs with p5.js 
 
 function setup() { //runs ONCE at the start
-  createCanvas(windowWidth, windowHeight);
+
+  createCanvas(400, 400);
+  print(windowWidth, windowHeight, width, height);
+  //windowWidth: current brower width
+  //width:       canvas width
 }
 
 function draw() { //runs OVER and OVER
-                  //targeting 60 frames per second
-  //background(220);
-  fill(mouseY,mouseX,0);
+  //targeting 60 frames per second
+  //STRIVE to keep draw() clean and tidy
+  background(220);  //draws a big solid rectangle
+  //0 - black    255 - white
+  drawTwoCircles();
+}
+
+function drawTwoCircles() { //[ALT][SHFT][F] → Autoformat
+  // draws two circles, one at a fixed location
+  // and one at the mouse location.
+
+  // Draw order:
+  // Later a thing is "drawn" (further down in draw())
+  // The higher it is in the draw stack
+
+  fill(255, 0, 255);
+  circle(width / 2, height / 2, 100);
+
+  fill(0, 255, 0); //colors are a bit like 
+  //picking up a marker, or
+  //filling a paintbrush with paint...
+  //R, G, B
   circle(mouseX, mouseY, 30);
+
+
 }
